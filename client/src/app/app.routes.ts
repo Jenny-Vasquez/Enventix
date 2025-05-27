e3094ee716a60493101aad93392317eb6c280ba5
Rocio Contarino <rconcan142@ieszaidinvergeles.org>
2025-05-27 19:53:02 +0200

    Cambios en el frontend

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,10 +1,21 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-
-    {
-        path: '',
-        loadChildren: ()=> import('./auth/auth.routes'),
-    }
+  {
+    path: '',
+    redirectTo: 'auth/welcome',
+    pathMatch: 'full',
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.routes').then(m => m.authRoutes),
+  },
+  {
+    path: 'event-front',
+    loadChildren: () => import('./event-front/event.routes').then(m => m.eventRoutes),
+  },
+  {
+    path: 'menu',
+     loadChildren: () => import('./menu/menu.routes').then(m => m.menuRoutes),
+  }
 ];
-
--- client/src/app/layout-front/layout-front/layout-front.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-layout-front',
-  imports: [],
-  templateUrl: './layout-front.component.html',
-  styleUrl: './layout-front.component.css',
-})
-export class LayoutFrontComponent { }
